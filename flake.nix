66e2102151125a788f5b78bdd20125e0f2f1ab29
Madeline Baggins <declanbaggins@gmail.com>
2025-08-14 15:59:57 +1000

    Refactor a little and add comments.

--- flake.nix
+++ flake.nix
@@ -1,22 +1,28 @@
 {
   inputs = {
+    # Nixpkgs
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     oxalica.url = "github:oxalica/rust-overlay";
+    # Rust overlay
     oxalica.inputs.nixpkgs.follows = "nixpkgs";
+    # Tools for building Elm packages
     elm-tools.url = "github:jeslie0/mkElmDerivation";
     elm-tools.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs = { self, nixpkgs, oxalica, elm-tools }: let
     overlays = [ elm-tools.overlays.mkElmDerivation oxalica.overlays.default ];
     pkgs  = system: import nixpkgs { inherit system overlays; };
+    # The rust package we use for our platform
     rust = system: with (pkgs system); rust-bin.stable.latest.default.override {
       extensions = [ "rust-src" "rust-analyzer" "rustfmt" "clippy" ];
       targets = [ "wasm32-unknown-unknown" ];
     };
+    # The rust platform we use to build our rust packages
     rsPlatform = system: with (pkgs system); makeRustPlatform {
       cargo = (rust system);
       rustc = (rust system);
     };
+    # The derivation that builds our wasm package 
     wasm = system: with (pkgs system); (rsPlatform system).buildRustPackage {
       pname = "recipe-site-wasm";
       version = "0.1.0";
@@ -32,6 +38,7 @@
         cp -r pkg/* $out
       '';
     };
+    # The derivation that builds javascript from our Elm code
     js = system: with (pkgs system); mkElmDerivation {
       name = "recipe-site-js";
       version = "0.1.0";
@@ -45,12 +52,15 @@
         cp main.js $out
       '';
     };
+    # The service binary
     package = system: (rsPlatform system).buildRustPackage {
       pname = "recipe-site";
       version = "0.2.0";
       src = ./rs;
       cargoLock.lockFile = ./rs/Cargo.lock;
     };
+    # A development shell that can build the service
+    # binary and all its components with make.
     devShell = system: with (pkgs system); mkShell {
       # All the Rust stuff comes from here
       inputsFrom = [ (package system) ];
@@ -69,10 +79,18 @@
         elm-format
       ];
     };
-  in {
+  in rec {
+    # Development Shells
     devShells.aarch64-linux.default = devShell "aarch64-linux";
+    devShells.x86_64-linux.default = devShell "x86_64-linux";
+    # Packages
+    packages.aarch64-linux.default = packages.aarch64-linux.service;
+    packages.aarch64-linux.service = package "aarch64-linux";
     packages.aarch64-linux.wasm = wasm "aarch64-linux";
     packages.aarch64-linux.js = js "aarch64-linux";
-    packages.aarch64-linux.default = package "aarch64-linux";
+    packages.x86_64-linux.default = packages.x86_64-linux.service;
+    packages.x86_64-linux.service = package "x86_64-linux";
+    packages.x86_64-linux.wasm = wasm "x86_64-linux";
+    packages.x86_64-linux.js = js "x86_64-linux";
   };
 }
